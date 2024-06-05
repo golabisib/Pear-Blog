89dcd095f54c694f43f6d9a2e874b75af854ba3d
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-06 01:23:23 +0330

    The project is connected to hyqraph API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { gql,useQuery } from "@apollo/client"
 
+const QUERY = gql`
+    query {
+        authors {name}
+    }
+`
 
 function App() {
-
-
+    const {loading, data} = useQuery(QUERY);
+    console.log(data)
   return (
     <>
-
+    <h1>App</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+	uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clwx4cx40072707tetzuppwid/master",
+	cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+);
