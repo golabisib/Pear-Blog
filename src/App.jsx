290f61b37796e2e9bcbf0539f61e5bfd75a62d50
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-12 14:19:48 +0330

    The MUI and NewTheme with new Font is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { gql,useQuery } from "@apollo/client"
+
 
 // components
 import Header from "./component/Header"
 
-const QUERY = gql`
-    query {
-        authors {name}
-    }
-`
+
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+import { ThemeProvider } from "@mui/material";
+import theme from "./MUI/theme.js";
+import App from "./App.jsx";
+
+import "./styles/index.css";
+import "./styles/fonts.css";
+
 const client = new ApolloClient({
-	uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clwx4cx40072707tetzuppwid/master",
-	cache: new InMemoryCache(),
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clwx4cx40072707tetzuppwid/master",
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
+  </React.StrictMode>
 );
