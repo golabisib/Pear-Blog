3b7cd27521b75decb9e17f59d4b9f07d1630ef45
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-18 23:07:02 +0330

    The card is compelete

--- src/component/shared/CardEl.jsx
+++ src/component/shared/CardEl.jsx
@@ -1,11 +1,89 @@
+import PropTypes from "prop-types";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Typography,
+} from "@mui/material";
+// import { Link } from "react-router-dom";
 
-function CardEl(props) {
-    console.log(props)
+function CardEl({ title, slug, coverPhoto, author }) {
   return (
     <div>
+      <Card
+        sx={{
+          boxShadow: "rgb(129, 190, 206) 0px 4px 9px",
+          borderRadius: "4px",
+        }}
+      >
+        <CardHeader
+          avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
+          title={
+            <Typography
+              component="p"
+              variant="p"
+              color="#012e4a"
+              fontSize="20px"
+            >
+              {author.name}
+            </Typography>
+          }
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={coverPhoto.url}
+          alt={slug}
+        />
 
+        <CardContent>
+          <Typography
+            component="h3"
+            variant="h6"
+            color="#012e4a"
+            fontWeight={600}
+          >
+            {title}
+          </Typography>
+        </CardContent>
+        <Divider variant="middle" sx={{ margin: "10px" }} />
+        <CardActions>
+          {/* <Link to={"/blogs/${slug}"} style={{textDecoration:"none", width: "100%"}} > */}
+            <Button
+              variant="outlined"
+              size="medium"
+              sx={{
+                width: "100%",
+                borderRadius: 3,
+                backgroundColor: "#036280",
+                color: "#e8ede7",
+                "&:hover": { backgroundColor: "#e8ede7", color: "#036280" },
+              }}
+            >
+              مطالعه مقاله
+            </Button>
+          {/* </Link> */}
+        </CardActions>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default CardEl
+CardEl.propTypes = {
+  author: PropTypes.shape({
+    avatar: PropTypes.shape({
+      url: PropTypes.any,
+    }),
+    name: PropTypes.any,
+  }),
+  coverPhoto: PropTypes.any,
+  slug: PropTypes.any,
+  title: PropTypes.any,
+};
+
+export default CardEl;
