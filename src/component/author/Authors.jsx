f5a13767f1298cabc60b408518cff71f7fc2fd09
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-19 00:01:52 +0330

    The author part is complete

--- src/component/author/Authors.jsx
+++ src/component/author/Authors.jsx
@@ -1,11 +1,53 @@
-
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
 
 function Authors() {
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+  if (loading) return <h3>Loading...</h3>;
+  if (errors) return <h3>Error</h3>;
+  console.log(data);
+  const { authors } = data;
   return (
-    <div>
-
-    </div>
-  )
+    <Grid
+      container
+      sx={{
+        boxShadow: "rgb(129, 190, 206) 0px 4px 7px",
+        borderRadius: "4px",
+      }}
+    >
+      {authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid item xs={12} padding={2}>
+            <a
+              href={`/authors/$author.slug`}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+              }}
+            >
+              <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
+              <Typography
+                component="p"
+                variant="p"
+                color="#012e4a"
+                fontSize="18px"
+              >
+                {author.name}
+              </Typography>
+            </a>
+          </Grid>
+          {index !== author.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
 }
 
-export default Authors
+export default Authors;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,23 +1,35 @@
 import { gql } from "@apollo/client";
 
-const GET_BLOGS_INFO = gql `
+const GET_BLOGS_INFO = gql`
   query MyQuery {
-  posts {
-    slug
-    id
-    author {
+    posts {
+      slug
+      id
+      author {
+        name
+        avatar {
+          url
+        }
+      }
+      title
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
+
+const GET_AUTHORS_INFO = gql`
+  query {
+    authors {
       name
+      id
+      slug
       avatar {
         url
       }
     }
-    title
-    coverPhoto {
-      url
-    }
   }
-}
-
 `;
 
-export {GET_BLOGS_INFO} ;
+export { GET_BLOGS_INFO, GET_AUTHORS_INFO };
